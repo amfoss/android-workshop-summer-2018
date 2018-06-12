ab5d42dbf5790a99a476ce1271a73f7ae8a0c7ee
Aaditya Menon <aaditya@sparker0i.me>
2018-06-12 13:11:26 +0530

    Day 11/Morning Session

--- Day 10/Weather/app/src/main/java/com/sparker0i/weather/MainActivity.java
+++ Day 10/Weather/app/src/main/java/com/sparker0i/weather/MainActivity.java
@@ -1,7 +1,10 @@
 package com.sparker0i.weather;
 
+import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.widget.TextView;
 
 import org.json.JSONException;
@@ -18,19 +21,28 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        String cityName = "Kayankulam";
+        String cityName = "Kannur";
         cityView = findViewById(R.id.city);
         tempView = findViewById(R.id.temp);
 
+        execute(new String[]{cityName});
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
 
     }
 
-    public void execute(String city) {
+    public void execute(String[] args) {
         JSONObject weather = null;
 
         WeatherTask task = new WeatherTask();
         try {
-            weather = task.execute(city).get();
+            if (args.length == 1)
+                weather = task.execute(args[0]).get();
+            else
+                weather = task.execute(args).get();
         }
         catch (InterruptedException iex) {
             iex.printStackTrace();
@@ -43,10 +55,30 @@ public class MainActivity extends AppCompatActivity {
             String cityName = weather.getString("name");
             Double temp = weather.getJSONObject("main").getDouble("temp");
 
-
+            cityView.setText(cityName);
+            tempView.setText(temp.toString());
         } catch (JSONException e) {
             e.printStackTrace();
         }
 
+    }
+
+    public void run() {
+
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.menu_weather , menu);
+        return super.onCreateOptionsMenu(menu);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        int id = item.getItemId();
+        if (id == R.id.gps) {
+            
+        }
+        return super.onOptionsItemSelected(item);
     }
 }
